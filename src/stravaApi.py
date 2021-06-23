fbfc278628b1ca58090b79f1b5d07a96655664d5
jackmleitch <jackmleitch@gmail.com>
2021-06-23 14:48:01 +0100

    formatting

--- src/stravaApi.py
+++ src/stravaApi.py
@@ -2,6 +2,7 @@ import pandas as pd
 import requests
 
 import urllib3
+
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
 from pandas import json_normalize
@@ -10,25 +11,25 @@ auth_url = "https://www.strava.com/oauth/token"
 activites_url = "https://www.strava.com/api/v3/athlete/activities"
 
 payload = {
-    'client_id': "50446",
-    'client_secret': '7955204b7285824280b5b30af6361996b414dc9f',
-    'refresh_token': '3d26f702b60d42195b267cd4029ed958143cb250',
-    'grant_type': "refresh_token",
-    'f': 'json'
+    "client_id": "50446",
+    "client_secret": "7955204b7285824280b5b30af6361996b414dc9f",
+    "refresh_token": "3d26f702b60d42195b267cd4029ed958143cb250",
+    "grant_type": "refresh_token",
+    "f": "json",
 }
 
 # check if there is a new request token
 print("Requesting Token...")
 res = requests.post(auth_url, data=payload, verify=False)
-access_token = res.json()['access_token']
+access_token = res.json()["access_token"]
 print("\tAccess Token = {}\n".format(access_token))
 
 
-header = {'Authorization': 'Bearer ' + access_token}
+header = {"Authorization": "Bearer " + access_token}
 
 # initialise dataframe with first 200 activities
 page_num = 1
-param = {'per_page': 200, 'page': page_num}
+param = {"per_page": 200, "page": page_num}
 my_dataset = requests.get(activites_url, headers=header, params=param).json()
 activities = json_normalize(my_dataset)
 
@@ -41,20 +42,16 @@ while True:
     if page_num % 5 == 0:
         print(f"Now extracting page number {page_num}")
 
-    param = {'per_page': 200, 'page': page_num}
+    param = {"per_page": 200, "page": page_num}
     my_dataset = requests.get(activites_url, headers=header, params=param).json()
     if my_dataset:
         new_activities = json_normalize(my_dataset)
         # activities.append(new_activities)
-        activities = activities.append(new_activities, ignore_index = True)
+        activities = activities.append(new_activities, ignore_index=True)
 
     else:
         break
 
 
 # save activities df to input folder
-activities.to_csv('./input/strava_activities.csv')
-
-
-
-
+activities.to_csv("./input/strava_activities.csv")
