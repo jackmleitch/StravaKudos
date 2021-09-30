885cca5c6d08340c1db79a1cf5d0b8e75f898a8f
Jack <jackmleitch@gmail.com>
2021-09-30 12:16:19 -0600

    model dispatch

--- src/model_dispatcher.py
+++ src/model_dispatcher.py
@@ -6,14 +6,15 @@ from sklearn import linear_model
 from sklearn import svm
 
 models = {
-    "decision_tree": tree.DecisionTreeRegressor(),
     "rf": ensemble.RandomForestRegressor(),
     "xgb": xgb.XGBRegressor(n_jobs=-1),
     "linear": linear_model.LinearRegression(),
-    "lasso": linear_model.Lasso(),
     "ridge": linear_model.Ridge(),
-    "svm": svm.SVR(),
-    "svm_linear": svm.SVR(kernel="linear"),
-    "svm_poly": svm.SVR(kernel="poly"),
-    "svm_sigmoid": svm.SVR(kernel="sigmoid"),
 }
+
+# "decision_tree": tree.DecisionTreeRegressor(),
+# "lasso": linear_model.Lasso(),
+# "svm": svm.SVR(),
+# "svm_linear": svm.SVR(kernel="linear"),
+# "svm_poly": svm.SVR(kernel="poly"),
+# "svm_sigmoid": svm.SVR(kernel="sigmoid"),
